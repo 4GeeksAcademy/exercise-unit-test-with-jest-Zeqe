c0f91546ec5413a7335f37fea773ef7221408bd8
ZeqeB <85196913+ezebellino@users.noreply.github.com>
2024-09-23 17:03:33 +0000

    terminando la guía de Jest

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-//console.log("Hello World")
+console.log("Hello World")
 const sum = (a,b) => {
-    return a+b;
+        return a+b;
 }
 
 
 console.log(sum(7,3))
 
-module.exports = {sum};
+let oneEuroIs = {
+    'JPY': 156.5,
+    'USD': 1.07,
+    'GBP': 0.87,
+};
+
+const fromEuroToDollar = (amount) => {
+    return amount * oneEuroIs.USD;
+}
+
+const fromDollarToYen = (amount) => {
+    let amountInEuro = amount / oneEuroIs.USD;
+    return amountInEuro * oneEuroIs.JPY;
+}
+
+const fromYenToPound = (amount) => {
+    let amountInEuro = amount / oneEuroIs.JPY;
+    return amountInEuro * oneEuroIs.GBP;
+};
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
--- test.js
+++ test.js
@@ -4,3 +4,17 @@ test('adds 14 + 9 to equal 23', () => {
     let total = sum(14, 9);
     expect(total).toBe(23);
 });
+
+const { fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
+
+test('converts 1 Euro to 1.07 USD', () => {
+    expect(fromEuroToDollar(1)).toBe(1.07);
+});
+
+test('converts 1 USD to 156.5 JPY', () => {
+    expect(fromDollarToYen(1)).toBeCloseTo(146.26, 2); // Usamos toBeCloseTo por posibles errores de redondeo
+});
+
+test('converts 1 JPY to 0.0056 GBP', () => {
+    expect(fromYenToPound(1)).toBeCloseTo(0.0056, 4); // Usamos toBeCloseTo por posibles errores de redondeo
+});
